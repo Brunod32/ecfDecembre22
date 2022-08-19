222d25b02462e911c59715fa4a6296fe0b9af540
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-08-19 14:16:28 +0200

    Added register and login for technicalTeam

---
+++ src/Controller/TechnicalTeamController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TechnicalTeamController extends AbstractController
+{
+    #[Route('/technicalteam', name: 'app_technical_team')]
+    public function index(): Response
+    {
+        return $this->render('technical_team/index.html.twig');
+    }
+}
